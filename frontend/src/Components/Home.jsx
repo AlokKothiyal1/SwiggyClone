178f45c73d2b9b3261bd61d73ae9e4f4fac5b489
Sheelu27 <sheelukumari27@gmail.com>
2020-11-08 08:21:17 +0530

    Minor Updates

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import Navigator from './Navigator';
-import Promotions from './Promotions';
-import SideMenu from './SideMenu';
-import HotelCard from './HotelCard';
-const data = require('../utils/data.json');
+import React from "react";
+import Navigator from "./Navigator";
+import Promotions from "./Promotions";
+import SideMenu from "./SideMenu";
+import HotelCard from "./HotelCard";
+import Sidebar from "./Sidebar";
+const data = require("../utils/data.json");
 // console.log(data.current);
 
 function Home() {
-    return (
-        <div>
-            <Navigator />
-            <Promotions />
-            <SideMenu />
-            <div className='container'>
-                <div className='row row-cols-3'>
-                    {data.current.map((item) => (
-                        <HotelCard data={item} key={item._id} />
-                    ))}
-                </div>
+  return (
+    <div>
+      <Navigator />
+      <Promotions />
+      <SideMenu />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-2">
+            <Sidebar />
+          </div>
+          <div className="col-md-10">
+            <div className="row row-cols-3 justify-content-center">
+              {data.current.map((item) => (
+                <HotelCard data={item} key={item._id} />
+              ))}
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Home;
