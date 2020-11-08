67e236e17f724c4e536fde1061c55b920c5e637b
Fahad Shaikh <fahadshaikh9776@gmail.com>
2020-11-09 01:58:34 +0530

    Sorting Functionality Added in All-Restaurants Section

--- frontend/src/Components/RestautantPage/Home.jsx
+++ frontend/src/Components/RestautantPage/Home.jsx
@@ -1,35 +1,37 @@
-import React from "react";
-import Navigator from "./Navigator";
-import Promotions from "./Promotions";
-import SideMenu from "./SideMenu";
-import HotelCard from "./HotelCard";
-import Sidebar from "./Sidebar";
-const data = require("../../utils/data.json");
+import React from 'react';
+import Navigator from './Navigator';
+import Promotions from './Promotions';
+import HotelCard from './HotelCard';
+import Sidebar from './Sidebar';
+import AllRestaurants from './AllRestaurants';
+import MainFooter from '../LandingPage/MainFooter';
+const data = require('../../utils/data.json');
 
 // console.log(data.current);
 
 function Home() {
-  return (
-    <div>
-      <Navigator />
-      <Promotions />
-      <SideMenu />
-      <div className="container">
-        <div className="row">
-          <div className="col-md-2">
-            <Sidebar />
-          </div>
-          <div className="col-md-10">
-            <div className="row row-cols-3 justify-content-center">
-              {data.current.map((item) => (
-                <HotelCard data={item} key={item._id} />
-              ))}
+    return (
+        <div>
+            <Navigator />
+            <Promotions />
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-2'>
+                        <Sidebar />
+                    </div>
+                    <div className='col-md-10'>
+                        <div className='row row-cols-3 justify-content-center'>
+                            {data.current.map((item) => (
+                                <HotelCard data={item} key={item._id} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <AllRestaurants />
+            <MainFooter />
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Home;
