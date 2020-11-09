6308868acc16f18f3982853e4becc2ade4b2cf8b
Sheelu27 <sheelukumari27@gmail.com>
2020-11-09 17:00:07 +0530

    Added MenuPage

--- frontend/src/Routes/Routes.jsx
+++ frontend/src/Routes/Routes.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Menu } from "@material-ui/core";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 // import LandingPg from '../Components/LandingPage/LandingPg';
-import Home from '../Components/RestautantPage/Home';
+import Home from "../Components/RestautantPage/Home";
 // import Restaurants from '../Components/Restaurants';
+import { MenuPages } from "../Components/MenuPage/MenuPages";
 
 export const Routes = () => {
-    return (
-        <>
-            <Switch>
-                {/* <Route path='/' exact render={() => <LandingPg />} /> */}
-                <Route path='/' render={() => <Home />} />
-                {/* <Route path='/Restaurants' render={() => <Home />} /> */}
-                {/* <Route path='/Restaurants' render={() => <Restaurants />} /> */}
-            </Switch>
-        </>
-    );
+  return (
+    <>
+      <Switch>
+        {/* <Route path='/' exact render={() => <LandingPg />} /> */}
+        {/* <Route path='/' render={() => <Home />} /> */}
+        {/* <Route path='/Restaurants' render={() => <Home />} /> */}
+        {/* <Route path='/Restaurants' render={() => <Restaurants />} /> */}
+        <Route path="/" render={() => <MenuPages />} />
+      </Switch>
+    </>
+  );
 };
